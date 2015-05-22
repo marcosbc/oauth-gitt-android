f7bb406e71863aa9a817ca7e1c16fe5970d83262
Marcos Bjoerkelund <marcosbjorkelund@gmail.com>
2015-05-22 20:19:12 +0200

    (luis) updates

--- ClienteBanco/app/src/main/java/luis/clientebanco/AppMainExample.java
+++ ClienteBanco/app/src/main/java/luis/clientebanco/AppMainExample.java
@@ -23,9 +23,23 @@ package luis.clientebanco;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import luis.clientebanco.OAuth.AppContext;
 import luis.clientebanco.OAuth.GenericDialogListener;
@@ -85,7 +99,7 @@ public class AppMainExample extends Activity {
 
 					accessCode = values.getString("code");
 
-					urbankAccessToken();
+					new urbankAccessToken().execute();
 					//JSONObject tokenJson = new JSONObject(tokenResponse);
 					//if(LOGGING.DEBUG)Log.d(TAG, "tokenJson->" + tokenJson);
 
@@ -107,6 +121,7 @@ public class AppMainExample extends Activity {
 
 	}
 
+	/*
 	public void urbankAccessToken(){
 
 		accessToken = "";
@@ -155,7 +170,116 @@ public class AppMainExample extends Activity {
 
 	}
 
+*/
+
+	public class urbankAccessToken extends AsyncTask<Void,Void,String>
+
+	{
+		@Override
+		protected String doInBackground(Void... params){
+
+			String URL = AppContext.UB_APP_TOKEN_URL
+					+ "?client_id=" + AppContext.UB_CLIENT_ID
+					+ "&redirect_uri=" + AppContext.UB_APP_REDIRECT
+					+ "&grant_type=authorization_code"
+					+ "&client_secret=" + AppContext.UB_CLIENT_SECRET
+					+ "&code=" + accessCode;
+
+			StringBuilder stringBuilder = new StringBuilder();
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpGet httpGet = new HttpGet(URL);
+
+			try {
+				HttpResponse response = httpClient.execute(httpGet);
+				StatusLine statusLine = response.getStatusLine();
+				int statusCode = statusLine.getStatusCode();
+				if (statusCode == 200) {
+					HttpEntity entity = response.getEntity();
+					InputStream inputStream = entity.getContent();
+					BufferedReader reader = new BufferedReader(
+							new InputStreamReader(inputStream));
+					String line;
+					while ((line = reader.readLine()) != null) {
+						stringBuilder.append(line);
+					}
+					inputStream.close();
+				} else {
+					Log.d("JSON", "Failed to download file");
+				}
+			} catch (Exception e) {
+				Log.d("readJSONFeed", e.getLocalizedMessage());
+			}
+
+			return stringBuilder.toString();
+		}
+
+		protected void onPostExecute(String result) {
+
+			try {
+				JSONObject tokens = new JSONObject(result);
+
+				accessToken = tokens.getString("access_token");
+				Log.d("Access", "Access Token");
+
+				new peticionAPI().execute();
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
 
 
+		}
+	}
+
+	public class peticionAPI extends AsyncTask<Void,Void,String>
+
+	{
+		@Override
+		protected String doInBackground(Void... params) {
+
+			String URL = "http://api.urbank.bjorkelund.me/tokeninfo"
+					+ "?access_token=" + accessToken;
+
+			StringBuilder stringBuilder = new StringBuilder();
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpGet httpGet = new HttpGet(URL);
+
+			try {
+				HttpResponse response = httpClient.execute(httpGet);
+				StatusLine statusLine = response.getStatusLine();
+				int statusCode = statusLine.getStatusCode();
+				if (statusCode == 200) {
+					HttpEntity entity = response.getEntity();
+					InputStream inputStream = entity.getContent();
+					BufferedReader reader = new BufferedReader(
+							new InputStreamReader(inputStream));
+					String line;
+					while ((line = reader.readLine()) != null) {
+						stringBuilder.append(line);
+					}
+					inputStream.close();
+				} else {
+					Log.d("JSON", "Failed to download file");
+				}
+			} catch (Exception e) {
+				Log.d("readJSONFeed", e.getLocalizedMessage());
+			}
+
+			return stringBuilder.toString();
+		}
+
+		protected void onPostExecute(String result) {
+
+			try {
+				JSONObject info = new JSONObject(result);
+
+				Log.d("Access", info.toString());
+				Log.d("Access", info.toString());
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+
+		}
+	}
 }
 
